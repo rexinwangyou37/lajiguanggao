689c0d406ed7189add5f16bbfecec39c3c09d5f1
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2022-11-11 17:44:58 +0800

    Update yuanyuke.js

--- JS/yuanyuke.js
+++ JS/yuanyuke.js
@@ -4,11 +4,11 @@
  * 
  */
 
-const url = ``;
+const url = `https://image.qqchess.qq.com/QQChessWX/public/assets/res/HuoDong/oldActivity/Hy_huodong_tankuang2.png`;
 const method = `GET`;
 const headers = {
 'User-Agent' : `%E5%85%83%E5%AE%87%E5%AE%A2/10251734 CFNetwork/1331.0.7 Darwin/21.4.0`,
-'Host' : `promote-pic.helipay.com`,
+'Host' : `image.qqchess.qq.com`,
 'Connection' : `keep-alive`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
 'Accept-Encoding' : `gzip, deflate, br`,
