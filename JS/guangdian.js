2ace757202ee53c17e01c4cdf41cef2bc7abfae2
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-01-16 20:54:08 +0800

    Update guangdian.js

--- JS/guangdian.js
+++ JS/guangdian.js
@@ -2,7 +2,6 @@ let body = JSON.parse($response.body)
     body.data.forEach(item => {
         if (item.posterStandName === "首页-弹窗广告") {
             item.adList = []
-            })
         }
     })
     $done({body: JSON.stringify(body)})
