21264b3ddb29249b59e3e99e640e02e5ccda9e6e
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-01-31 14:01:48 +0800

    Create 51xinyongka.js

---
+++ JS/51xinyongka.js
@@ -0,0 +1,24 @@
+let url = $request.url
+let method = $request.method
+let body = null
+
+switch (true) {
+    case url.includes("api.u51.com/generic-config-gateway/api"):
+        let obj = JSON.parse($response.body)
+        if (obj.meTabConfigExts) {
+            let reserve = ['设置', '我的账单']
+            obj.meTabConfigExts = obj.meTabConfigExts.filter(item => reserve.includes(item.meTabConfigs[0].title))
+            obj.operationResourceDTO = undefined
+            body = JSON.stringify(obj)
+        }
+        break
+    default:
+        console.log("匹配到其他url：\n" + url)
+        break
+}
+
+if (body) {
+    $done({body})
+} else {
+    $done({})
+}
