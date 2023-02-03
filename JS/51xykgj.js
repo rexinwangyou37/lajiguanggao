15e61ce390abdcd161ae4c46f81ca6e6d3d97573
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-02-03 12:02:34 +0800

    Update 51xykgj.js

--- JS/51xykgj.js
+++ JS/51xykgj.js
@@ -1,4 +1,3 @@
-
 if (!$response.body) {
   $done({});
 }
@@ -7,14 +6,16 @@ let obj = JSON.parse($response.body);
 
 if (obj?.operationResourceDTO) {
   for (let i = 0; i < obj.operationResourceDTO.length; i++) {
-    obj.operationResourceDTO[i].endTime = 2040-01-02 00:00:00;
-    obj.operationResourceDTO[i].startTime = 2040-01-01 00:00:00;
+    obj.operationResourceDTO[i].endTime = "2040-01-02 00:00:00";
+    obj.operationResourceDTO[i].startTime = "2040-01-01 00:00:00";
   }
 }
 
-if (obj?.meTabConfigExts) {
-  let reserve = ['设置', '我的账单']
-  obj.meTabConfigExts = obj.meTabConfigExts.filter(item => reserve.includes(item.meTabConfigs[0].title));
+if (url.includes("api.u51.com/generic-config-gateway/api")) {
+  if (obj?.meTabConfigExts) {
+    let reserve = ['设置', '我的账单']
+    obj.meTabConfigExts = obj.meTabConfigExts.filter(item => reserve.includes(item.meTabConfigs[0].title));
+  }
 }
 
 body = JSON.stringify(obj);
