5df09d0cf3408e50c92fdab02d843f3d122fff08
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-02-03 11:34:19 +0800

    Create 51xykgj.js

---
+++ JS/51xykgj.js
@@ -0,0 +1,21 @@
+
+if (!$response.body) {
+  $done({});
+}
+const url = $request.url;
+let obj = JSON.parse($response.body);
+
+if (obj?.operationResourceDTO) {
+  for (let i = 0; i < obj.operationResourceDTO.length; i++) {
+    obj.operationResourceDTO[i].endTime = 2040-01-02 00:00:00;
+    obj.operationResourceDTO[i].startTime = 2040-01-01 00:00:00;
+  }
+}
+
+if (obj?.meTabConfigExts) {
+  let reserve = ['设置', '我的账单']
+  obj.meTabConfigExts = obj.meTabConfigExts.filter(item => reserve.includes(item.meTabConfigs[0].title));
+}
+
+body = JSON.stringify(obj);
+$done({body});
