be56a15572592516119afcb330981f6b7b38f1e0
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-02-22 09:21:28 +0800

    Update 12306.js

--- JS/12306.js
+++ JS/12306.js
@@ -1,18 +1,3 @@
+const version = 'V1.0.20';
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var ddgksf2013=JSON.parse($response.body);ddgksf2013.materialsList&&(1==ddgksf2013.materialsList.length?(ddgksf2013.materialsList[0].filePath="https://api.dujin.org/bing/m.php",ddgksf2013.advertParam.skipTime=1,delete ddgksf2013.materialsList[0].billId,ddgksf2013.materialsList[0].billMaterialsId="6491"):ddgksf2013.materialsList.length>1&&(ddgksf2013.materialsList=[{}])),$done({body:JSON.stringify(ddgksf2013)});
+var obj=JSON.parse($request.body),ddgksf2013={};"0007"==obj.placementNo?(ddgksf2013.materialsList=[{billMaterialsId:"6491",filePath:"ddgksf2013",creativeType:1}],ddgksf2013.advertParam={skipTime:1},ddgksf2013.code="00"):ddgksf2013={code:"00",message:"无广告返回"},$done({body:JSON.stringify(ddgksf2013)});
