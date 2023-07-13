4eae7f46c3c1e07733194912afefd5ed7641f32b
rexinwangyou37 <117501309+rexinwangyou37@users.noreply.github.com>
2023-07-13 12:23:59 +0800

    Update jhsh.js

--- JS/jhsh.js
+++ JS/jhsh.js
@@ -1,8 +1,13 @@
 if ($response.内容) {
   var obj = JSON.parse($response.内容);
+  if (obj.TAG_AD_INFO)｛
+    console.log("去除广告1");
     obj.TAG_AD_INFO = [];
+  }
+  if (obj.MEBCT_AD_INFO)｛
+    console.log("去除广告2");
     obj.MEBCT_AD_INFO = [];
-
+  }
   $done({ 内容: JSON.stringify(obj) });
 } else {
   $done({})
